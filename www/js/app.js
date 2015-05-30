e57023e2891a02dcf7bdcae758c62dd3eeeed811
Gregory Haskins <greg@greghaskins.com>
2015-05-30 13:19:53 -0400

    now dogfooding its own statuses

--- www/js/app.js
+++ www/js/app.js
@@ -13,14 +13,62 @@ angular.module('sidewinder-app', ['ionic'])
   });
 })
 
-.controller('StatusController', function($scope, $timeout, $ionicModal) {
+.controller('StatusController', function($scope, RepoAssessor) {
+  var repositories = [
 
-  $scope.repoStatuses = [{
-    'name': 'first thing',
-    'state': 'success',
-  }, {
-    'name': 'second thing',
-    'state': 'failed',
-  }];
+    {
+      owner: 'sidewinder-team',
+      name: 'sidewinder-server',
+    }, {
+      owner: 'sidewinder-team',
+      name: 'sidewinder-ios',
+    }, {
+      owner: 'greghaskins',
+      name: 'sidewinder-app',
+    },
 
+  ];
+  $scope.repoAssessments = [];
+  repositories.forEach(function(repository) {
+    RepoAssessor.assess(repository).then(function(assessment) {
+      $scope.repoAssessments.push(assessment);
+    }, function(reason) {
+      console.error(reason);
+    });
+
+  });
+
+})
+
+.factory('RepoAssessor', function($http, $q) {
+  return {
+    assess: function(repository) {
+      var repo = repository.owner + "/" + repository.name;
+      var url = "https://api.github.com/repos/" + repo + "/statuses/master";
+
+      var deferred = $q.defer();
+      $http.get(url).success(function(data) {
+        if (data.length < 1) {
+          deferred.resolve({
+            name: repo,
+            state: 'unknown',
+          });
+        }
+
+        var status = data[0];
+        deferred.resolve({
+          name: repo,
+          state: status.state,
+        });
+      }).error(function() {
+        colsole.error('Failed to get statuses from ' + url);
+        deferred.resolve({
+          name: repo,
+          state: 'unknown',
+        });
+      });
+
+      return deferred.promise;
+    }
+  };
 })
