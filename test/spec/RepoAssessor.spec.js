94dfa0f85c68268bbd4ea701f43634fd7b897a95
Gregory Haskins <greg@greghaskins.com>
2015-06-05 01:05:58 -0400

    add test coverage around RepoAssessor

---
+++ test/spec/RepoAssessor.spec.js
@@ -0,0 +1,50 @@
+describe('the RepoAssessor', function() {
+
+    beforeEach(module('sidewinder-app'));
+
+    var $httpBackend, RepoAssessor;
+
+    beforeEach(inject(function(_$httpBackend_, _RepoAssessor_, _GitHubRepo_) {
+        $httpBackend = _$httpBackend_;
+        RepoAssessor = _RepoAssessor_;
+        GitHubRepo = _GitHubRepo_;
+    }));
+
+    it('gets the repository state from GitHub combined status API', function(done) {
+        $httpBackend.whenGET('https://api.github.com/repos/sidewinder-team/sidewinder-server/commits/master/status')
+            .respond(200, {
+                state: 'success',
+            });
+
+        var repo = GitHubRepo('sidewinder-team', 'sidewinder-server');
+
+        RepoAssessor.assess(repo).then(function(result) {
+            expect(result.state).toBe('success');
+        }).catch(function(error) {
+            expect(error).toBeUndefined();
+        }).finally(done);
+
+        $httpBackend.flush();
+
+    });
+
+    it('returns repository state of pending when connection fails', function(done) {
+
+        $httpBackend.whenGET('https://api.github.com/repos/angular/angular.js/commits/master/status')
+            .respond(500, {
+                boom: 'goes the dynamite'
+            });
+
+        var repo = GitHubRepo('angular', 'angular.js');
+
+        RepoAssessor.assess(repo).then(function(result) {
+            expect(result.state).toBe('pending');
+        }).catch(function(error) {
+            expect(error).toBeUndefined();
+        }).finally(done);
+
+        $httpBackend.flush();
+
+    });
+
+});
--- www/js/app.js
+++ www/js/app.js
@@ -1,78 +1,82 @@
 angular.module('sidewinder-app', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.controller('StatusController', function($scope, RepoAssessor) {
-  var repositories = [
+.controller('StatusController', function($scope, GitHubRepo, RepoAssessor) {
+    var repositories = [
 
-    {
-      owner: 'sidewinder-team',
-      name: 'sidewinder-server',
-    }, {
-      owner: 'sidewinder-team',
-      name: 'sidewinder-ios',
-    }, {
-      owner: 'greghaskins',
-      name: 'sidewinder-app',
-    },
+        GitHubRepo('sidewinder-team', 'sidewinder-server'),
+        GitHubRepo('sidewinder-team', 'sidewinder-ios'),
+        GitHubRepo('greghaskins', 'sidewinder-app'),
 
-  ];
-  $scope.refresh = function() {
-    $scope.repoAssessments = [];
-    repositories.forEach(function(repository) {
-      RepoAssessor.assess(repository).then(function(assessment) {
-        $scope.repoAssessments.push(assessment);
-      }, function(reason) {
-        console.error(reason);
-      });
+    ];
+    $scope.refresh = function() {
+        $scope.repoAssessments = [];
+        repositories.forEach(function(repository) {
+            RepoAssessor.assess(repository).then(function(assessment) {
+                    assessment.name = repository.fullName;
+                    $scope.repoAssessments.push(assessment);
+                },
+                function(reason) {
+                    console.error(reason);
+                });
 
-    });
-  }
-  $scope.refresh();
+        });
+    }
+    $scope.refresh();
 
 })
 
 .factory('RepoAssessor', function($http, $q) {
-  return {
-    assess: function(repository) {
-      var repo = repository.owner + "/" + repository.name;
-      var url = "https://api.github.com/repos/" + repo + "/statuses/master";
+    return {
+        assess: function(repository) {
+            var repo = repository.owner + "/" + repository.name;
+            var url = "https://api.github.com/repos/" + repo + "/commits/master/status";
 
-      var deferred = $q.defer();
-      $http.get(url).success(function(data) {
-        if (data.length < 1) {
-          deferred.resolve({
-            name: repo,
-            state: 'unknown',
-          });
-          return;
+            var deferred = $q.defer();
+            $http.get(url).success(function(data) {
+
+                deferred.resolve({
+                    state: data.state,
+                });
+
+            }).error(function() {
+
+                deferred.resolve({
+                    state: 'pending',
+                });
+
+            });
+
+            return deferred.promise;
         }
-
-        var status = data[0];
-        deferred.resolve({
-          name: repo,
-          state: status.state,
-        });
-      }).error(function() {
-        colsole.error('Failed to get statuses from ' + url);
-        deferred.resolve({
-          name: repo,
-          state: 'unknown',
-        });
-      });
-
-      return deferred.promise;
-    }
-  };
+    };
 })
+
+.factory('GitHubRepo', function() {
+    function GitHubRepo(owner, name) {
+        var repo = {
+            owner: owner,
+            name: name
+        };
+        Object.defineProperty(repo, 'fullName', {
+            get: function() {
+                return repo.owner + '/' + repo.name;
+            },
+        });
+        return repo;
+    };
+
+    return GitHubRepo;
+});
